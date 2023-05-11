4d2d13fe966957dfbafca844583a63738046b431
Dipro <dipro@MacBook-Air-cua-Dipro-27.local>
2023-05-11 09:27:03 +0700

    Fix validate data

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express()
 const port = 3000
 
 app.get('/',async (req, res) => {
+    if(!eq.query.lang || eq.query.text){
+        res.send("")
+        return 
+    }
     const { text } = await translate(req.query.text, { to: req.query.lang });
     console.log(text) // => 'Hello World! How are you?'
     res.send(text)
