f8c547788131a279e72eaaabeb4ea9d4e35dfe48
Dipro <dipro@MacBook-Air-cua-Dipro-27.local>
2023-05-11 09:21:52 +0700

    first-commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { translate } = require('./google-translate-api/dist/cjs/index');
+ 
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',async (req, res) => {
+    const { text } = await translate(req.query.text, { to: req.query.lang });
+    console.log(text) // => 'Hello World! How are you?'
+    res.send(text)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
