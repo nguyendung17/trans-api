08857f9488b0a9080da546b3f48d870a5a8a861d
Dipro <dipro@MacBook-Air-cua-Dipro-27.local>
2023-05-12 11:40:08 +0700

    Code

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const { translate } = require('./google-translate-api/dist/cjs/index');
- 
+
+const fs = require('fs');
+const { translate } = require('bing-translate-api');
+
+const langs =JSON.parse( fs.readFileSync("./lang.json"),"utf-8");
 
 const express = require('express')
 const app = express()
@@ -7,14 +10,28 @@ const port = 3000
 
 app.get('/',async (req, res) => {
     try {
-        if(!req.query.lang || !req.query.text){
+        let {lang,text} = req.query;
+        if(!lang || !text){
             res.status(500);
             res.send("err")
             return 
         }
-        const { text } = await translate(req.query.text, { to: req.query.lang });
-        console.log(text) // => 'Hello World! How are you?'
-        res.send(text)
+
+        if(langs.filter((it)=>{return it.code==lang}).length==0){
+            res.status(500)("No Support code: "+lang,500)
+            return;
+        }
+        
+        translate(req.query.text, null, req.query.lang).then(result => {
+            res.send(result.translation);
+          }).catch(err => {
+            console.error(err);
+            res.status(500);
+            res.send("err")
+
+          });
+        
+
     } catch (error) {
         res.status(500);
         console.log(error);
@@ -23,5 +40,5 @@ app.get('/',async (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
