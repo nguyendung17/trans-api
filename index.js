c82180002c6c9dac58a85e1ec3e7920ca555bc46
Dipro <dipro@MacBook-Air-cua-Dipro-27.local>
2023-05-12 15:36:11 +0700

    cache

--- index.js
+++ index.js
@@ -2,43 +2,53 @@
 const fs = require('fs');
 const { translate } = require('bing-translate-api');
 
-const langs =JSON.parse( fs.readFileSync("./lang.json"),"utf-8");
+const langs = JSON.parse(fs.readFileSync("./lang.json"), "utf-8");
 
 const express = require('express')
 const app = express()
 const port = 3000
+const Cached = {};
+async function trans(text, lang) {
+    let key = `${text}_${lang}`;
+    if( Cached[key] ){
+        console.log("Cached");
+        return Cached[key];
+    } 
+    return new Promise((ok, nok) => {
+        translate(text, null, lang).then(result => {
+            Cached[key] = result;
+            ok(result);
+        }).catch(err => {
+           nok(err);
+        });
+    })
 
-app.get('/',async (req, res) => {
+}
+
+app.get('/', async (req, res) => {
     try {
-        let {lang,text} = req.query;
-        if(!lang || !text){
+        let { lang, text } = req.query;
+        if (!lang || !text) {
             res.status(500);
             res.send("err")
-            return 
+            return
         }
 
-        if(langs.filter((it)=>{return it.code==lang}).length==0){
-            res.status(500).send("No Support code: "+lang,500)
+        if (langs.filter((it) => { return it.code == lang }).length == 0) {
+            res.status(500).send("No Support code: " + lang, 500)
             return;
         }
-        
-        translate(req.query.text, null, req.query.lang).then(result => {
-            res.send(result.translation);
-          }).catch(err => {
-            console.error(err);
-            res.status(500);
-            res.send("err")
 
-          });
-        
+        res.send(await trans(text,lang))
+
 
     } catch (error) {
         res.status(500);
         console.log(error);
     }
-   
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+    console.log(`Example app listening on port http://localhost:${port}`)
 })
